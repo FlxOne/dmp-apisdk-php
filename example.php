9b784363fd0de46b58dc652add8f8ae0b4a063e9
PimVerlangen <pim.verlangen@flxone.com>
2016-03-22 15:00:21 +0100

    Changed example.php

--- example.php
+++ example.php
@@ -22,51 +22,47 @@ $client = new Client($config);
 // Now we can execute calls to the Teradata DMP API
 
 // 1: A GET request to user/current to get the current user's information
+$service = 'user/current';
+$request = new Request($service);
 try {
-    $service = 'user/current';
-    $request = new Request($service);
     $response = $client->get($request);
 } catch (ClientException $ex) {
     echo 'An exception has occurred when executing this request: ' . $ex->getMessage();
 }
 
 // 2: A POST request to create a new data collection pixel
+$service = 'tracking/beacon';
+$request = new Request($service);
+$request->setParameter('name', 'SDK-created Pixel');
+$request->setParameter('type', 'smart_pixel');
 $pixelId = -1; // Store the pixelId for further usage in PUT/DELETE examples
 try {
-    $service = 'tracking/beacon';
-    $request = new Request($service);
-    $request->setParameter('name', 'SDK-created Pixel');
-    $request->setParameter('type', 'pixel');
     $response = $client->post($request);
-
     // Store the id of the created pixel
     $pixelId = $response->get('beacon')['id'];
-
     echo 'Created a new pixel with id [' . $pixelId . '] and name: [' . $response->get('beacon')['name'] . ']' . PHP_EOL;
 } catch (ClientException $ex) {
     echo 'An exception has occurred when executing this request: ' . $ex->getMessage();
 }
 
 // 3: A PUT request to change the name of the previously created data collection pixel
+$service = 'tracking/beacon';
+$request = new Request($service);
+$request->setParameter('id', $pixelId);
+$request->setParameter('name', 'SDK-created Pixel has had it\'s name changed');
 try {
-    $service = 'tracking/beacon';
-    $request = new Request($service);
-    $request->setParameter('id', $pixelId);
-    $request->setParameter('name', 'SDK-created Pixel has had it\'s name changed');
     $response = $client->put($request);
-
     echo 'Changed the name of the pixel with id [' . $pixelId . '] to: [' . $response->get('beacon')['name'] . ']' . PHP_EOL;
 } catch (ClientException $ex) {
     echo 'An exception has occurred when executing this request: ' . $ex->getMessage();
 }
 
 // 4: A DELETE request to delete the previously created data collection pixel
+$service = 'tracking/beacon';
+$request = new Request($service);
+$request->setParameter('id', $pixelId);
 try {
-    $service = 'tracking/beacon';
-    $request = new Request($service);
-    $request->setParameter('id', $pixelId);
     $response = $client->delete($request);
-
     echo 'Did we delete the previously created data collection pixel (true/false)? --> ' . (bool)$response->get('deleted') . PHP_EOL;
 } catch (ClientException $ex) {
     echo 'An exception has occurred when executing this request: ' . $ex->getMessage();
